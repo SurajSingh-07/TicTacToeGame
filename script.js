d7e69ef1f5be704a62533da348e0ecb02df28bb3
SurajSingh <er.suraj.singh0007@gmail.com>
2024-02-19 00:18:41 +0530

    completed

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+let audioTurn = new Audio("ting.mp3")
+
+let turn = "X"
+let isgameover = false;
+
+const changeTurn = ()=>{
+    return turn === "X"? "0": "X"
+}
+
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2, 4, 5, 0],
+        [3, 4, 5, 4, 15, 0],
+        [6, 7, 8, 4, 25, 0],
+        [0, 3, 6, -6, 15, 90],
+        [1, 4, 7, 4, 15, 90],
+        [2, 5, 8, 14, 15, 90],
+        [0, 4, 8, 4, 15, 45],
+        [2, 4, 6, 4, 15, 135],
+    ]
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "" )){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            isgameover = true;
+            document.querySelector(".imgBox").getElementsByTagName("img")[0].style.width = "150px"
+            document.querySelector(".line").style.width = "22vw";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+        }
+    }) 
+}
+
+let  boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector(".boxtext");
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            if(!isgameover){
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
+        }
+    }) 
+})
+
+reset.addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X";
+    isgameover = false;
+    document.querySelector(".line").style.width = "0vw";
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector(".imgBox").getElementsByTagName("img")[0].style.width = "0px"
+})
